6187c8474499f0a935ba9a5fb99c611ba4512d64
fifer0s <96656932+fifer0s@users.noreply.github.com>
2024-10-08 00:13:38 +0100

    Refactor: Simplify configureYargs function and improve error handling

--- utils/config.ts
+++ utils/config.ts
@@ -1,6 +1,5 @@
 import yargs from "yargs";
 import { hideBin } from "yargs/helpers";
-
 import logger from "./logger";
 
 /**
@@ -11,40 +10,41 @@ import logger from "./logger";
 export function configureYargs() {
   return yargs(hideBin(process.argv))
     .strict() // Ensure only defined options are accepted
-    .option("path", {
-      alias: "p",
-      type: "string",
-      description: "Project path",
-      demandOption: true,
-    })
-    .option("detector", {
-      alias: "r",
-      type: "string",
-      description: "Specify which detector to run",
-    })
-    .option("verbose", {
-      alias: "v",
-      type: "boolean",
-      description: "Enable verbose logging",
-      default: false,
-    })
-    .option("recursive", {
-      alias: "R",
-      type: "boolean",
-      description: "Parse projects recursively",
-      default: false,
-    })
-    .option("output", {
-      alias: "o",
-      type: "string",
-      description: "Specify output file",
+    .options({
+      path: {
+        alias: "p",
+        type: "string",
+        description: "Project path",
+        demandOption: true,
+      },
+      detector: {
+        alias: "d",
+        type: "string",
+        description: "Specify which detector to run",
+      },
+      verbose: {
+        alias: "v",
+        type: "boolean",
+        description: "Enable verbose logging",
+        default: false,
+      },
+      recursive: {
+        alias: "r",
+        type: "boolean",
+        description: "Parse projects recursively",
+        default: false,
+      },
+      output: {
+        alias: "o",
+        type: "string",
+        description: "Specify output file",
+      },
     })
     .fail((msg, err, yargs) => {
-      if (err) {
-        console.error(`Error: ${err.message}`);
-      } else {
-        console.error(`Error: ${msg || "Unknown argument provided."}`);
-      }
+      const errorMessage = err
+        ? `Error: ${err.message}`
+        : `Error: ${msg || "Unknown argument provided."}`;
+      console.error(errorMessage);
       console.log(yargs.help());
       process.exit(1);
     })
@@ -63,7 +63,5 @@ export function configureYargs() {
  * @param {boolean} verbose - Flag to enable verbose logging.
  */
 export function setupLogger(verbose: boolean) {
-  if (verbose) {
-    logger.level = "debug";
-  }
+  logger.level = verbose ? "debug" : "info";
 }
