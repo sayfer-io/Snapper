f9ca989c2428a33c1c3c82fa42dc8facbffb0b69
fifer0s <177220384+fifer0s@users.noreply.github.com>
2024-10-10 16:32:40 +0100

    Refactor: Update jest.config.js to use rootDir in testPathIgnorePatterns

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
   testMatch: ["**/__tests__/**/*.test.ts"],
-  testPathIgnorePatterns: ["/node_modules/", "/testcases/"],
+  testPathIgnorePatterns: ["<rootDir>/node_modules/", "<rootDir>/testcases/"],
   moduleFileExtensions: ["ts", "js", "json", "node"],
   transform: {
     "^.+\\.tsx?$": ["ts-jest", { tsconfig: "tsconfig.json" }],
