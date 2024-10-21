3d9f16d4752d11dac962d76feca74e29efda40b1
fifer0s <177220384+fifer0s@users.noreply.github.com>
2024-10-21 09:52:10 +0100

    Refactor jest.config.js to add moduleDirectories option

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,7 @@ export default {
   testMatch: ["**/__tests__/**/*.test.ts"],
   testPathIgnorePatterns: ["<rootDir>/node_modules/", "<rootDir>/testcases/"],
   modulePathIgnorePatterns: ["<rootDir>/testcases/"],
+  moduleDirectories: ["node_modules"],
   moduleFileExtensions: ["ts", "js", "json", "node"],
   transform: {
     "^.+\\.tsx?$": ["ts-jest", { tsconfig: "tsconfig.json" }],
