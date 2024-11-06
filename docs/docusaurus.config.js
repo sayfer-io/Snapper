0412ebf53fbb4d8cf1fcfe0d5b93a71a9751ee11
asifqatar <asifdeveloper230@gmail.com>
2024-11-06 17:46:39 +0500

    fix: docs logo changes

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -23,7 +23,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
-          editUrl: 'https://github.com/facebook/docusaurus/edit/main/website/',
+          editUrl: 'https://github.com/sayfer-io/Snapper/blob/main/',
         },
         blog: {
           showReadingTime: true,
