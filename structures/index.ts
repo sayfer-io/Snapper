696d23f79a52c0340fd19d80bea46d5aec37b185
Saldat0 <9461116-saldat00@users.noreply.gitlab.com>
2024-11-20 00:19:23 +0000

    Enhance documentation for risk rating, impact, confidence, and ease of exploitation enums with detailed descriptions

--- structures/index.ts
+++ structures/index.ts
@@ -1,5 +1,10 @@
 /**
  * Enum representing the risk rating of a finding.
+ *
+ * This enum defines the possible risk ratings for a finding, ranging from
+ * "Informational" (lowest risk) to "Critical" (highest risk). These risk
+ * ratings are often used to prioritize and address identified issues in the
+ * source code.
  */
 export enum RiskRating {
   Informational = 0,
@@ -10,7 +15,12 @@ export enum RiskRating {
 }
 
 /**
- * Enum representing impact of a finding.
+ * Enum representing the impact of a finding.
+ *
+ * This enum defines the possible impact levels for a finding, ranging from
+ * "Informational" (lowest impact) to "Critical" (highest impact). The impact
+ * level is a measure of how severely the finding could affect the system or
+ * its users.
  */
 export enum Impact {
   Informational = 0,
@@ -22,6 +32,11 @@ export enum Impact {
 
 /**
  * Enum representing the confidence level of a finding.
+ *
+ * This enum defines the possible confidence levels for a finding, ranging from
+ * "Low" (lowest confidence) to "High" (highest confidence). The confidence
+ * level indicates the degree of certainty that the finding is accurate and
+ * represents a real issue.
  */
 export enum Confidence {
   Low = 0,
@@ -31,6 +46,11 @@ export enum Confidence {
 
 /**
  * Enum representing the ease of exploitation of a finding.
+ *
+ * This enum defines the possible levels of ease of exploitation for a finding,
+ * ranging from "Easy" (lowest difficulty) to "Hard" (highest difficulty). The
+ * ease of exploitation indicates how difficult it would be for an attacker to
+ * exploit the identified vulnerability or issue.
  */
 export enum EaseOfExploitation {
   Easy = 0,
