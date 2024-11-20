10a628ed59edecdaf97f853defaea2c0d9832c51
Saldat0 <9461116-saldat00@users.noreply.gitlab.com>
2024-11-20 00:13:13 +0000

    Enhance Finding interface documentation with detailed property descriptions

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,7 +1,17 @@
-import { RiskRating } from "../structuress";
+import { RiskRating } from "../structures";
 
 /**
  * Represents a finding in the source code.
+ *
+ * This interface defines the structure of a finding, which is a detected issue or
+ * problem within the source code. It includes the following properties:
+ *
+ * - `type`: The type or category of the finding (e.g., "security vulnerability", "code smell").
+ * - `description`: A textual description of the finding, providing more details about the issue.
+ * - `position`: An object that contains information about the location of the finding in the source code:
+ *   - `filePath`: The full path to the file where the finding is located.
+ *   - `lineNum`: The line number within the file where the finding is located.
+ * - `riskRating`: The risk rating associated with the finding, which is an enum of type `RiskRating`.
  */
 export type Finding = {
   /**
