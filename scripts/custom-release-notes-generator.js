ca97f148017ed3d995605c8008fb089d0f4c4a4d
Saldat0 <9461116-saldat00@users.noreply.gitlab.com>
2024-12-05 07:40:26 +0000

    fix: refactor custom release notes generator to use dynamic import and improve commit transformation logic

--- scripts/custom-release-notes-generator.js
+++ scripts/custom-release-notes-generator.js
@@ -1,49 +1,52 @@
-const releaseNotesGenerator = require("@semantic-release/release-notes-generator");
+(async () => {
+  const releaseNotesGenerator = await import(
+    "@semantic-release/release-notes-generator"
+  );
 
-module.exports = {
-  ...releaseNotesGenerator,
-  writerOpts: {
-    transform: (commit, context) => {
-      const issues = [];
-      commit.notes.forEach((note) => {
-        note.title = "BREAKING CHANGES";
-      });
-      if (commit.type === "feat") {
-        commit.type = "Features";
-      } else if (commit.type === "fix") {
-        commit.type = "Bug Fixes";
-      } else if (commit.type === "chore") {
-        commit.type = "Chores";
-      } else if (commit.type === "refactor") {
-        commit.type = "Refactors";
-      } else if (commit.type === "test") {
-        commit.type = "Tests";
-      } else if (commit.type === "ci") {
-        commit.type = "CI/CD";
-      } else if (!commit.type || commit.type === "*") {
-        commit.type = "Miscellaneous";
-      }
-      if (commit.scope === "*") {
-        commit.scope = "";
-      }
-      if (typeof commit.hash === "string") {
-        commit.shortHash = commit.hash.substring(0, 7);
-      }
-      if (typeof commit.subject === "string") {
-        const match = commit.subject.match(/#(\d+)/g);
-        if (match) {
-          match.forEach((issue) => {
-            issues.push(issue);
-          });
+  module.exports = {
+    ...releaseNotesGenerator,
+    writerOpts: {
+      transform: (commit, context) => {
+        const issues = [];
+        commit.notes.forEach((note) => {
+          note.title = "BREAKING CHANGES";
+        });
+        if (commit.type === "feat") {
+          commit.type = "Features";
+        } else if (commit.type === "fix") {
+          commit.type = "Bug Fixes";
+        } else if (commit.type === "chore") {
+          commit.type = "Chores";
+        } else if (commit.type === "refactor") {
+          commit.type = "Refactors";
+        } else if (commit.type === "test") {
+          commit.type = "Tests";
+        } else if (commit.type === "ci") {
+          commit.type = "CI/CD";
+        } else if (!commit.type || commit.type === "*") {
+          commit.type = "Miscellaneous";
         }
-        commit.subject = commit.subject.replace(
-          /#(\d+)/g,
-          (_, issue) =>
-            `[#${issue}](https://github.com/${context.owner}/${context.repository}/issues/${issue})`
-        );
-      }
-      commit.references = commit.references.concat(issues);
-      return commit;
+        if (commit.scope === "*") {
+          commit.scope = "";
+        }
+        if (typeof commit.hash === "string") {
+          commit.shortHash = commit.hash.substring(0, 7);
+        }
+        if (typeof commit.subject === "string") {
+          const match = commit.subject.match(/#(\d+)/g);
+          if (match) {
+            match.forEach((issue) => {
+              issues.push(issue);
+            });
+          }
+          commit.subject = commit.subject.replace(
+            /#(\d+)/g,
+            (_, issue) =>
+              `[#${issue}](https://github.com/${context.owner}/${context.repository}/issues/${issue})`
+          );
+        }
+        return commit;
+      },
     },
-  },
-};
+  };
+})();
